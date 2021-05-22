1929c36e9169289c6576213dc6ed353916b6a292
victor kigen <vickigen@gmail.com>
2021-05-22 23:03:13 +0300

    added comment model

--- instaclone/models.py
+++ instaclone/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+# from cloudinary.models import CloudinaryField
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    # cloudinary_image = CloudinaryField("image", null=True)
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
@@ -35,6 +38,7 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
+    # cloudinary_image = CloudinaryField("image", null=True)
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
     caption = models.CharField(max_length=250, blank=True)
@@ -84,3 +88,4 @@ class Follow(models.Model):
 
     def __str__(self):
         return f'{self.follower} Follow'
+
